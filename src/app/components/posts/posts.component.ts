e0ec9ad8fc3b9865b836ec0c7e39f7a23bd69d7d
Irshad Ansari <irshadjsr21@gmail.com>
2018-10-02 23:06:54 +0530

    Added Navbar Active Styles

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-posts',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  posts:Object;
+
+  constructor(private dataService:DataService) { }
 
   ngOnInit() {
+    this.dataService.getPosts().subscribe(data => this.posts=data);
   }
 
 }
