6bff51dab03c426c5f3714782e23648b055a3528
Irshad Ansari <irshadjsr21@gmail.com>
2018-10-02 22:10:14 +0530

    Added Components and services with basic sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { UsersComponent } from './components/users/users.component';
+import { DetailsComponent } from './components/details/details.component';
+import { PostsComponent } from './components/posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    UsersComponent,
+    DetailsComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
